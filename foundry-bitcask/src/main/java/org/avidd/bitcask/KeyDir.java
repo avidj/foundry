ccdf19a78f95408f2394ff5f9f6fb68f4109a271
David Kensche <3089531+avidj@users.noreply.github.com>
2026-06-01 14:46:32 +0200

    keydir as index

--- foundry-bitcask/src/main/java/org/avidd/bitcask/KeyDir.java
+++ foundry-bitcask/src/main/java/org/avidd/bitcask/KeyDir.java
@@ -1,4 +1,10 @@
 package org.avidd.bitcask;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class KeyDir {
+  private final Map<String, Entry> map = new HashMap<>();
+
+  public record Entry(long fileId, long valueOffset, int valueSize, long timestamp) {}
 }
